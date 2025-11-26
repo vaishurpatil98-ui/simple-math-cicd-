edc8d10c03d70fa29c3d2dd1993869b6d9d78995
vaishali123 <vaishurpatil98@gmail.com>
2025-11-26 16:26:21 +0530

    first commit

---
+++ tests/test_math_ops.py
@@ -0,0 +1,10 @@
+from src.math_ops import add,subtract,multiply
+
+def test_add():
+    assert add(2,3)==5
+
+def test_subtract():
+    assert subtract(5,2)==3
+
+def test_multiply():
+    assert multiply(3,4)==12
